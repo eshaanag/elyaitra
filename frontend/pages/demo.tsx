6507b94bf26e2d1ec8c61556a89a0872dca43d8c
eshaanag <agrawaleshaan12@gmail.com>
2025-12-17 22:59:21 +0530

    Update iframe title in DemoPage component

--- frontend/pages/demo.tsx
+++ frontend/pages/demo.tsx
@@ -2,12 +2,8 @@ export default function DemoPage() {
   return (
     <iframe
       src="/demo/index.html"
-      style={{
-        width: "100%",
-        height: "100vh",
-        border: "none",
-      }}
-      title="Elyaitra Demo"
+      style={{ width: "100%", height: "100vh", border: "none" }}
+      title="Demo"
     />
   );
 }
