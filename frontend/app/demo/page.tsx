4ed685a04d49213cc57902786651804f0563c5f2
eshaanag <agrawaleshaan12@gmail.com>
2025-12-17 21:56:00 +0530

    Update page.tsx

--- frontend/app/demo/page.tsx
+++ frontend/app/demo/page.tsx
@@ -1,14 +1,8 @@
-import fs from "fs";
-import path from "path";
-
 export default function DemoPage() {
-  const htmlPath = path.join(process.cwd(), "public/demo/index.html");
-  const html = fs.readFileSync(htmlPath, "utf8");
-
   return (
-    <div
-      dangerouslySetInnerHTML={{ __html: html }}
-      style={{ width: "100%", height: "100%" }}
-    />
+    <div style={{ padding: 40 }}>
+      <h1>DEMO PAGE WORKS</h1>
+      <p>If you see this, routing is fixed.</p>
+    </div>
   );
 }
