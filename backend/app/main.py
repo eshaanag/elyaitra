f742ab341502505e634d0014fce4230c2a40fc6f
Ashu8810 <agrawaleshaan12@gmail.com>
2026-01-02 11:01:09 +0530

    update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -35,7 +35,3 @@ app.include_router(content_router)
 app.include_router(payments_router)
 app.include_router(access_router)
 
-if __name__ == "__main__":
-    import uvicorn
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
