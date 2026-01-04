09bfabc5ad5debc7deeed28093b81525b23c26f2
eshaanag <agrawaleshaan12@gmail.com>
2026-01-04 16:13:02 +0530

    update llm_client.py

--- backend/app/ai_engine/llm_client.py
+++ backend/app/ai_engine/llm_client.py
@@ -2,9 +2,6 @@ import os
 from dotenv import load_dotenv
 import google.generativeai as genai
 
-# --------------------------------------------------
-# ENV
-# --------------------------------------------------
 load_dotenv()
 
 API_KEY = os.getenv("GEMINI_API_KEY")
